fa3312b4d01e2daa03b26a8e2f6507ed48bafef8
iurynovarino <novarinos@hotmail.com>
2023-07-04 22:43:58 -0300

    add dicionario.py

---
+++ Dicionarios.py
@@ -0,0 +1,7 @@
+contatos = {
+   "joão@mcdonalds.com": {"nome": "joão", "telefone": "1234-1234"},
+   "adonis@rocky.com": {"nome": "Adonis", "telefone": "4321-1234"},
+    "apollo@nasa.com": {"nome": "Apollo", "telefone": "1234-4321"},
+    "creed@chuchu.com": {"nome": "Creed", "telefone": "4321-4321"}
+}
+print(contatos["apollo@nasa.com"]["telefone"])
